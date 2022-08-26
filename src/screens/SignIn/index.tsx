75e4ec37b6f7304fe1cc2986fc5aeff0256d6b29
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-08-26 19:42:09 -0300

    chore: added scroll/keyboard avoiding view components for accessibility

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
 
 export function SignIn() {
   return (
     <Container>
-      <Input
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
-      <Input placeholder="Password" type="secondary" secureTextEntry />
-      <Button title="Sign in" type="secondary" />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <Content>
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <Input placeholder="Password" type="secondary" secureTextEntry />
+          <Button title="Sign in" type="secondary" />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,11 +1,22 @@
 import styled from 'styled-components/native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-export const Container = styled(LinearGradient).attrs(({ theme }) => ({
+export const Container = styled<any>(LinearGradient).attrs(({ theme }) => ({
   colors: theme.COLORS.GRADIENT,
   start: { x: 0, y: 1 },
   end: { x: 0.5, y: 0.5 },
 }))`
   flex: 1;
   justify-content: center;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace() + 48,
+  },
+})`
+  width: 100%;
+  padding: 0 32px;
 `;
