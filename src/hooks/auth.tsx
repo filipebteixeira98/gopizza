be8bbb5c5ec50282869b44bed5f0335b5fd3f43c
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-09-01 11:57:58 -0300

    chore: retrieved user profile data from firestore & added to state

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,10 +1,18 @@
 import React, { createContext, useContext, ReactNode, useState } from 'react';
 import { Alert } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+
+type User = {
+  id: string;
+  name: string;
+  isAdmin: boolean;
+};
 
 type AuthContextData = {
   signIn: (email: string, password: string) => Promise<void>;
   isLogging: boolean;
+  user: User | null;
 };
 
 type AuthProviderProps = {
@@ -15,6 +23,7 @@ export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
   const [isLogging, setIsLogging] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
 
   async function signIn(email: string, password: string) {
     if (!email || !password) {
@@ -25,7 +34,30 @@ function AuthProvider({ children }: AuthProviderProps) {
 
     auth()
       .signInWithEmailAndPassword(email, password)
-      .then((account) => console.log(account))
+      .then((account) => {
+        firestore()
+          .collection('users')
+          .doc(account.user.uid)
+          .get()
+          .then((profile) => {
+            const { name, isAdmin } = profile.data() as User;
+
+            if (profile.exists) {
+              const userData = {
+                id: account.user.uid,
+                name,
+                isAdmin,
+              };
+
+              console.log(userData);
+
+              setUser(userData);
+            }
+          })
+          .catch(() =>
+            Alert.alert('Login', 'Could not find user profile data')
+          );
+      })
       .catch((error) => {
         const { code } = error;
 
@@ -39,7 +71,7 @@ function AuthProvider({ children }: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isLogging }}>
+    <AuthContext.Provider value={{ signIn, isLogging, user }}>
       {children}
     </AuthContext.Provider>
   );
