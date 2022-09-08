79bfaaa2636aff769e36c4401b4c39579de2cc42
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-09-08 14:53:25 -0300

    chore: created input price component

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -14,6 +14,7 @@ import {
 
 import { ButtonBack } from '@components/ButtonBack';
 import { Photo } from '@components/Photo';
+import { InputPrice } from '@components/InputPrice';
 
 export function Product() {
   const [image, setImage] = useState('');
@@ -50,6 +51,9 @@ export function Product() {
           type="secondary"
         />
       </Upload>
+      <InputPrice size="P" />
+      <InputPrice size="M" />
+      <InputPrice size="G" />
     </Container>
   );
 }
