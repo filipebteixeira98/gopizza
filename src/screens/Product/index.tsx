315337488770e8934d9fa812decc83ddc72d32ce
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-09-08 11:59:37 -0300

    chore: created photo component

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -5,6 +5,7 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Container, Header, Title, DeleteLabel } from './styles';
 
 import { ButtonBack } from '@components/ButtonBack';
+import { Photo } from '@components/Photo';
 
 export function Product() {
   return (
@@ -16,6 +17,7 @@ export function Product() {
           <DeleteLabel>Delete</DeleteLabel>
         </TouchableOpacity>
       </Header>
+      <Photo uri="" />
     </Container>
   );
 }
