8b0e39d97cab7c66dce77ded78d55f486584d46a
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-09-09 12:12:23 -0300

    chore: created home screen & added header to it

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useTheme } from 'styled-components/native';
+
+import {
+  Container,
+  Header,
+  Greeting,
+  GreetingEmoji,
+  GreetingText,
+} from './styles';
+
+import happyEmoji from '@assets/happy.png';
+
+export function Home() {
+  const { COLORS } = useTheme();
+
+  return (
+    <Container>
+      <Header>
+        <Greeting>
+          <GreetingEmoji source={happyEmoji} />
+          <GreetingText>Hello, Admin</GreetingText>
+        </Greeting>
+        <TouchableOpacity>
+          <MaterialIcons name="logout" color={COLORS.TITLE} size={24} />
+        </TouchableOpacity>
+      </Header>
+    </Container>
+  );
+}
