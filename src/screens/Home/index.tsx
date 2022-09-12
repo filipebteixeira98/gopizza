57600cda2e4cdb26bb5c5beefa408bafacb1d3b1
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-09-12 12:09:57 -0300

    chore: retrieved registered pizzas from firestore

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { useEffect } from 'react';
+import { Alert, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { useTheme } from 'styled-components/native';
+import firestore from '@react-native-firebase/firestore';
 
 import {
   Container,
@@ -15,12 +16,38 @@ import {
 } from './styles';
 
 import { Search } from '@components/Search';
-import { ProductCard } from '@components/ProductCard';
+import { ProductCard, ProductProps } from '@components/ProductCard';
 
 import happyEmoji from '@assets/happy.png';
 
 export function Home() {
   const { COLORS } = useTheme();
+
+  function fetchPizzas(value: string) {
+    const formattedValue = value.toLowerCase().trim();
+
+    firestore()
+      .collection('pizzas')
+      .orderBy('name_insensitive')
+      .startAt(formattedValue)
+      .endAt(`${formattedValue}\uf8ff`)
+      .get()
+      .then((response) => {
+        const data = response.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+
+        console.log(data);
+      })
+      .catch(() => Alert.alert('Query', 'Could not resolve query'));
+  }
+
+  useEffect(() => {
+    fetchPizzas('');
+  }, []);
 
   return (
     <Container>
