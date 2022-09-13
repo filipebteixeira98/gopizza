edc52382b4d7d2a4e45d59fa8208374e80930c5a
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-09-13 15:09:41 -0300

    feat: created new product button component & added function that user can navigate to register page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -14,6 +14,7 @@ import {
   Title,
   MenuHeader,
   MenuItemsNumber,
+  NewProductButton,
 } from './styles';
 
 import { Search } from '@components/Search';
@@ -66,6 +67,10 @@ export function Home() {
     navigation.navigate('product', { id });
   }
 
+  function handleAdd() {
+    navigation.navigate('product', {});
+  }
+
   useEffect(() => {
     fetchPizzas('');
   }, []);
@@ -103,6 +108,11 @@ export function Home() {
           paddingBottom: 125,
           marginHorizontal: 24,
         }}
+      />
+      <NewProductButton
+        title="Register Pizza"
+        type="secondary"
+        onPress={handleAdd}
       />
     </Container>
   );
