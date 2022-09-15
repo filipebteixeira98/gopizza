4af581d6ef7f974bd9b45ac3ff9414f6441c9dfe
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-09-15 09:15:52 -0300

    chore: created radio button component & added to order page

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Platform } from 'react-native';
 
-import { Container, Header, Photo } from './styles';
+import { Container, Header, Photo, Sizes } from './styles';
 
 import { ButtonBack } from '@components/ButtonBack';
+import { RadioButton } from '@components/RadioButton';
 
 export function Order() {
   return (
@@ -12,6 +13,9 @@ export function Order() {
         <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
       </Header>
       <Photo source={{ uri: 'https://github.com/filipebteixeira98.png' }} />
+      <Sizes>
+        <RadioButton title="Small" selected={false} />
+      </Sizes>
     </Container>
   );
 }
