8bd5a7b41f8de7b818b5a7ce74e646a678c356f7
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-09-15 10:02:31 -0300

    chore: created form component & added to order page

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,10 +1,24 @@
 import React, { useState } from 'react';
 import { Platform } from 'react-native';
 
-import { Container, Header, Photo, Sizes } from './styles';
+import {
+  Container,
+  ContentScroll,
+  Header,
+  Photo,
+  Sizes,
+  Form,
+  Title,
+  Label,
+  InputGroup,
+  FormRow,
+  Price,
+} from './styles';
 
 import { ButtonBack } from '@components/ButtonBack';
 import { RadioButton } from '@components/RadioButton';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 import { PIZZA_TYPES } from '@utils/pizzaTypes';
 
@@ -13,20 +27,38 @@ export function Order() {
 
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-      <Header>
-        <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
-      </Header>
-      <Photo source={{ uri: 'https://github.com/filipebteixeira98.png' }} />
-      <Sizes>
-        {PIZZA_TYPES.map((item) => (
-          <RadioButton
-            key={item.id}
-            title={item.name}
-            selected={size === item.id}
-            onPress={() => setSize(item.id)}
-          />
-        ))}
-      </Sizes>
+      <ContentScroll>
+        <Header>
+          <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
+        </Header>
+        <Photo source={{ uri: 'https://github.com/filipebteixeira98.png' }} />
+        <Form>
+          <Title>Pizza name</Title>
+          <Label>Select a size</Label>
+          <Sizes>
+            {PIZZA_TYPES.map((item) => (
+              <RadioButton
+                key={item.id}
+                title={item.name}
+                selected={size === item.id}
+                onPress={() => setSize(item.id)}
+              />
+            ))}
+          </Sizes>
+          <FormRow>
+            <InputGroup>
+              <Label>Table number</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+            <InputGroup>
+              <Label>Amount</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+          </FormRow>
+          <Price>Total of $00.00</Price>
+          <Button title="Confirm Order" />
+        </Form>
+      </ContentScroll>
     </Container>
   );
 }
